6a399e79c081007318e1243d4d58b3d0211656e5
tanveertkd <hazarikatanveer12@gmail.com>
2021-07-25 21:05:31 +0530

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+const textInput = document.querySelector(".input-textarea");
+const textOutput = document.querySelector(".output-textarea");
+const btnTranslate = document.querySelector(".btn-translate");
+
+const URL = "https://api.funtranslations.com/translate/";
+
+const data = textInput.value;
+console.log(data);
+
