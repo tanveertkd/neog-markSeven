c614ee3a989388e590eaced5aa2e545b774a585e
tanveertkd <hazarikatanveer12@gmail.com>
2021-07-25 21:50:05 +0530

    done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # neog-markSeven
-A translator that translates English to Vulcan.
+##  Klingon translator 
+<br />
+A translator that translates English to Klingon. It's a part of neoG.Camp's levelZero assignments. 
+
+Assignment: markSeven
--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const textInput = document.querySelector(".input-textarea");
 const textOutput = document.querySelector(".output-textarea");
 const btnTranslate = document.querySelector(".btn-translate");
 
-const URL = "https://api.funtranslations.com/translate/";
+const URL = "https://api.funtranslations.com/translate/vulcan.json";
 
-const data = textInput.value;
-console.log(data);
+const urlBuilder = (input) => {
+    return URL+"?text="+input;
+}
 
+const errorHandler = (error) => {
+    console.log("Looks like there's been an issue. Please try again later.", error);
+}
+
+const clickHandler = () => {
+    const data = textInput.value;
+    console.log(data);
+
+    fetch(urlBuilder(data))
+    .then(response => response.json())
+    .then(json => {
+        const responseData = json.contents.translated;
+        textOutput.innerText = responseData;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
